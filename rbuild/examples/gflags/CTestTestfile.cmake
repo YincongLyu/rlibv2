5efad6ed6f615c977b362a13c7208b5bcebfc76e
YincongLyu <yinconglyu@foxmail.com>
2024-01-01 23:09:21 -0800

    only test rc_pingpong & upgrade dirver to 5.7

--- rbuild/examples/gflags/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/yincong/github_proj/rdma_learning/rlibv2/examples/gflags
-# Build directory: /home/yincong/github_proj/rdma_learning/rlibv2/rbuild/examples/gflags
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
